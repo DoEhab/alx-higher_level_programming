b330cc87aad252bda721a58bdbc0783e71b76be5
DoEHab <dohaehab@gmail.com>
2024-09-12 09:52:57 -0700

    task 2

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    result = 0
+    arg_len = len(sys.argv)
+    if arg_len > 0:
+        for i in range(arg_len - 1):
+             result += int(sys.argv[i + 1])
+
+        print("{}".format(result))
