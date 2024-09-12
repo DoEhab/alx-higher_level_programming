904770971b24ff19ebde16479b0e54a02a3b9751
DoEHab <dohaehab@gmail.com>
2024-09-12 09:00:10 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+arg_len = len(sys.argv)
+if arg_len == 1:
+    print("{0 arguments.}")
+else:
+    print("{} arguments.".format(arg_len - 1))
+    for i in range(1, arg_len):
+        print("{}:{}".format(i, sys.argv[i]))
