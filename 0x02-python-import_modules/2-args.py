183b31a6f73d1fdbfddc5ac9a134c25fb6d6f4c8
DoEHab <dohaehab@gmail.com>
2024-09-12 09:10:04 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__"
+    import sys
 
-arg_len = len(sys.argv)
-if arg_len == 1:
-    print("{0 arguments.}")
-else:
-    print("{} arguments:".format(arg_len - 1))
-    for i in range(1, arg_len):
-        print("{}:{}".format(i, sys.argv[i]))
+    arg_len = len(sys.argv)
+    if arg_len == 1:
+        print("{0 arguments.}")
+    else:
+        print("{} arguments:".format(arg_len - 1))
+        for i in range(1, arg_len):
+            print("{}:{}".format(i, sys.argv[i]))
