ae034fb6a417dc4db682ddc2471a78f314dbdeb8
dohaehab <dohaehab@gmail.com>
2024-09-19 00:57:46 +0300

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    result_val = 0
+    result_key = ""
+    if a_dictionary is None or len(a_dictionary) == 0 :
+        return None
+    for k, v in a_dictionary.items():
+        if v > result_val:
+            result_val = v
+            result_key = k
+    return result_key
