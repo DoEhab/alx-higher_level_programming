9a1e59c7e1e039dd0984a3092f27f3d4bcd8c20e
dohaehab <dohaehab@gmail.com>
2024-09-19 00:58:25 +0300

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     result_val = 0
     result_key = ""
-    if a_dictionary is None or len(a_dictionary) == 0 :
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     for k, v in a_dictionary.items():
         if v > result_val:
