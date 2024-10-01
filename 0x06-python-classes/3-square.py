7af84cbd91396dfab1e8dd6055f50ef7db96857f
dohaehab <dohaehab@gmail.com>
2024-10-01 23:19:07 +0300

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+# File: 3-square.py
+"""Define Square Python Class."""
+
+
 class Square:
     def __init__(self, size=0):
+        """
+        new square
+        Args:
+            size: square size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
