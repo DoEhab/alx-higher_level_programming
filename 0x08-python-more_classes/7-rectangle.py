07f87b39c8d2f0d60908b352766b4d637eb46ed4
dohaehab <dohaehab@gmail.com>
2024-10-08 23:54:47 +0300

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -70,6 +70,8 @@ class Rectangle:
         for i in range(0, self.__height):
             for j in range(0, self.__width):
                 result.append(str(self.print_symbol))
+            if i < self.__height - 1:
+                result.append("\n")
         return ("".join(result))
 
     def __repr__(self):
