80c52a87c36060922e8096b618247a67d00c8914
dohaehab <dohaehab@gmail.com>
2024-10-10 14:11:47 +0300

    unit test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-
+"""define add_integer function"""
 def add_integer(a, b=98):
+    """ sum two numbers
+
+    Args:
+        a: first int
+        b: second int
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
