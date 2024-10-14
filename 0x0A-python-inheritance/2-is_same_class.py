f394d1b3aee7d8b3d0cd8cda8c3c2defda1b7eba
dohaehab <dohaehab@gmail.com>
2024-10-14 21:54:03 +0300

    t2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File: 2-is_same_class.py
+"""define fun is same class"""
+
+
+def is_same_class(obj, a_class):
+    """return true if obj is instance of class"""
+    return isinstance(obj, a_class)
