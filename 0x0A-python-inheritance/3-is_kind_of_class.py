b86af4e86aad8b5780fa8ff1e6fc53b866d7afff
dohaehab <dohaehab@gmail.com>
2024-10-14 22:07:54 +0300

    t3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# File: 3-is_kind_of_class
+"""define fun is kind of class"""
+def is_kind_of_class(obj, a_class):
+    """
+    function check if obj is instance of class
+    Args:
+        obj: the object to be checked
+        a_class: class to check if the obj is instance of
+
+    Returns:
+        if instance - True
+        otherwise - False
+    """
+    return (isinstance(obj, a_class))
