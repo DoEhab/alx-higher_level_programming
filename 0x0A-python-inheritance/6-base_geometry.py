4ef773d5c1e627a05e34991a2b7ed681d3ea46c9
dohaehab <dohaehab@gmail.com>
2024-10-14 22:27:27 +0300

    t6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define base geometry class"""
+
+
+class BaseGeometry:
+    """Geometry Class"""
+
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
