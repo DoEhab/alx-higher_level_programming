9d9b17213b9827475ede5e7fa4120be933191391
dohaehab <dohaehab@gmail.com>
2024-10-16 05:37:00 +0300

    t7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,10 @@
 """script to load json to list and save to file"""
 import sys
 
+save_json = __import__('5-save_to_json_file').save_to_json_file
+load_json = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    save_json = __import__('5-save_to_json_file').save_to_json_file
-    load_json = __import__('6-load_from_json_file').load_from_json_file
-
     my_list = []
 
     try:
@@ -14,5 +13,5 @@ if __name__ == "__main__":
     except FileNotFoundError:
         my_list = []
 
-    my_list.extend(load_json(sys.argv[1:]))
+    my_list.append(sys.argv[1:])
     save_json(my_list)
