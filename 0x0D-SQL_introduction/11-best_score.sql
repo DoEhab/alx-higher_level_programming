ba9495925f05462b4e5ad83710ac79aaf8d3ec1d
doehab <dohaehab@gmail.com>
2024-11-21 18:03:32 -0800

    task 11

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- list score and name where score greatear than or equal 10
-SELECT `score`, `name` FROM second_table WHERE `score` >= 10;
+SELECT `score`, `name` FROM second_table WHERE `score` >= 10 ORDER BY `score` DESC;
