89537318898aa62220c15b2bdb19baaaa5b6e768
doehab <dohaehab@gmail.com>
2024-11-21 19:51:19 -0800

    task 8

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,4 @@
+-- Print cities where state is california
+SELECT `id`, `name` FROM `hbtn_0d_usa`.`cities` WHERE `state_id` IN
+(SELECT `id` FROM `hbtn_0d_usa`.`states` WHERE `name` = "California")
+ORDER BY `id`;
