b5edd4ca8298d9e7a75ab85376383bcd6d8af049
doehab <dohaehab@gmail.com>
2024-11-21 17:43:23 -0800

    first table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS `second_table`(
 	`id` INT, 
 	`name` VARCHAR(256),
 	`score` INT);
-INSERT INTO second_table values
-(1, "john", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8)
+INSERT INTO second_table values (1, "john", 10);
+INSERT INTO second_table values (2, "Alex", 3);
+INSERT INTO second_table values (3, "Bob", 14);
+INSERT INTO second_table values (4, "George", 8);
