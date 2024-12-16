8a884e1d26e3cda693dac43d91a4dd354fc7fc91
dohaehab <dohaehab@gmail.com>
2024-12-16 23:00:02 +0200

    task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const argsLength = process.argv.slice(2).length;
 argsLength === 0 ? console.log('No argument') : argsLength === 1 ? console.log('Argument found') : console.log('Arguments found');
