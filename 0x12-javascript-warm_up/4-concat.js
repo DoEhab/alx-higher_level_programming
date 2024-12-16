3c80a33aac7a41766930e67b42927863c30d6d0b
dohaehab <dohaehab@gmail.com>
2024-12-16 23:22:01 +0200

    task 5

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+if (process.argv[2]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (!process.argv[2]) {
+  console.log('undefined is undefined');
+}
