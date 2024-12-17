1b1f20eedd87206470f9c4a4ecd5f060e6797119
dohaehab <dohaehab@gmail.com>
2024-12-17 20:16:12 +0200

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+if (!isNaN(process.argv[2])) {
+  let i = process.argv[2];
+  let j = i;
+  while (i > 0) {
+    console.log('X'.repeat(j));
+    i--;
+  }
+} else {
+  console.log('Missing size');
+}
