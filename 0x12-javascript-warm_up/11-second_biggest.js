48441b5688c1962c6dad70de7dd2024ff046f28c
dohaehab <dohaehab@gmail.com>
2024-12-17 23:13:58 +0200

    task 9

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2);
 if (args.length === 0 || args.length === 1) {
-    console.log(0);
-    return;
+  console.log(0);
+  process.exit(0);
 }
 args.sort();
 console.log(args[args.length - 2]);
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
-const i = Number(process.argv[2]);
-const j = Number(process.argv[3]);
-
-add = function (a, b) {
-  console.log(a + b);
-}
-
-add(i, j);
+module.exports = function add (a, b) {
+  return a + b;
+};
