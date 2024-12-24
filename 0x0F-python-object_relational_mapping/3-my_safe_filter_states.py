882e2ac505cb7d9557dce347046b5279a0fdd62e
doehab <dohaehab@gmail.com>
2024-12-24 22:28:06 +0200

    task 1

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,9 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE `name` = '" + state_name + "' ORDER BY `id` ASC")
+    cursor.execute(
+        "SELECT * FROM `states` WHERE `name` = %s ORDER BY `id` ASC", (state_name,)
+    )
 
     for row in cursor.fetchall():
         print(row)
