9d354854098ef4a0574ac0f0f9d8d93f1e11918a
doehab <dohaehab@gmail.com>
2024-12-24 21:05:35 +0200

    task 1

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#list all states
+#usage: /2-my_filter_states.py <mysql username>
+# <mysql password>
+# <database name>
+# <state name>
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                        port=3306,
+                        user=username,
+                        passwd=password,
+                        db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` WHERE name LIKE %s", (state_name,))
+
+    for row in cursor.fetchall():
+        print(row)
