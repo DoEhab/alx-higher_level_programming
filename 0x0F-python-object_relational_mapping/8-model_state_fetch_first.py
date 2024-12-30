e658a990cef6ed7661f0f30a582b0342ab6bba5a
doehab <dohaehab@gmail.com>
2024-12-30 23:32:06 +0200

    task 9

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,17 +3,17 @@
 print the first state and nothing if empty table
 """
 import sys
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]),
-        pool_pre_ping=True)
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
