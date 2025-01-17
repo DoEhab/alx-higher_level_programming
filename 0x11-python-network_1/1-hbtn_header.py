73cc5c5c7161729860332dfcd4244ef3b1ff6ced
doehab <dohaehab@gmail.com>
2025-01-17 11:16:52 -0800

    task 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+""" this module print url reponse  header X-Request-Id"""
 import urllib.request
 import sys
-""" print url response header X-Request-Id"""
+
+
 if __name__ == "__main__":
     """ print url response header X-Request-Id """
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         header = response.headers
         print(header.get('X-Request-Id'))
+
