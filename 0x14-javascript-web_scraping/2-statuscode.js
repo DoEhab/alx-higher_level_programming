44e6d0f32bb3e73eef4721da50e87d09b2660271
dohaehab <dohaehab@gmail.com>
2025-02-12 00:11:18 +0200

    task 1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = new Request(process.argv[2], {
 });
 
 fetch(request).then((response) => {
-  console.log({ code: response.status.code });
+  console.log({ code: response.status });
 });
