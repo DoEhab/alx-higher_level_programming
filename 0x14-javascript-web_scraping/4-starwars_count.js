24942460e50109866bfade57cb3ce810629f613c
dohaehab <dohaehab@gmail.com>
2025-02-12 01:00:08 +0200

    task 0

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (!error) {
+    let counter = 0;
+    const data = JSON.parse(body);
+    data.results.forEach(element => {
+      element.characters.forEach((characterUrl) => {
+        // Check if the character URL ends with "/18/"
+        if (characterUrl.includes('/18/')) {
+          counter++;
+        }
+      });
+    });
+    console.log(counter);
+  }
 });
