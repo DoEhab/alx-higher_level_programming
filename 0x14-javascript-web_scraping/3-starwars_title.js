9b5cdc3fc2ab8391134493efa97671e2fd741973
dohaehab <dohaehab@gmail.com>
2025-02-12 00:27:38 +0200

    task 0

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const { url } = require('inspector');
 const request = require('request');
-movie_id = process.argv[2];
-url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
+const movie_id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movie_id}`;
 request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
