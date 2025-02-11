74c56e3182c6cdc9d9123b572e56e27e570348de
dohaehab <dohaehab@gmail.com>
2025-02-11 23:21:16 +0200

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,22 @@
+
+if (process.argv[2]) {
+  try {
+    const fileName = process.argv[2];
+    const fileStream = require('fs');
+    const fileContent = fileStream.readFileSync(fileName, 'utf-8');
+    fileContent.split(/\r?\n/).forEach(line => {
+      console.log(line);
+    });
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log(
+        {
+          error: err.message,
+          errno: err.errno,
+          code: err.code,
+          syscall: err.syscall,
+          path: err.path
+        });
+    }
+  }
+}
