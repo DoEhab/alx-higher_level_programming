413729a2d4321e08de331b1193f9089d1875c23d
dohaehab <dohaehab@gmail.com>
2025-02-11 23:50:39 +0200

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
-});
+if (process.argv[2]) {
+  try {
+    const fileName = process.argv[2];
+    const fileStream = require('fs');
+    const fileContent = fileStream.readFileSync(fileName, 'utf-8');
+    fileContent.split(/\r?\n/).forEach(line => {
+      console.log(line);
+    });
+  } catch (err) {
+      console.log(err);
+  }
+}
