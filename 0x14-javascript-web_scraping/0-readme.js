2032aabb37213b10a0753579f42b4cc317b3adeb
dohaehab <dohaehab@gmail.com>
2025-02-11 23:41:14 +0200

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,23 +1,11 @@
 #!/usr/bin/node
-
+const fileStream = require('fs');
 if (process.argv[2]) {
-  try {
-    const fileName = process.argv[2];
-    const fileStream = require('fs');
-    const fileContent = fileStream.readFileSync(fileName, 'utf-8');
-    fileContent.split(/\r?\n/).forEach(line => {
-      console.log(line);
-    });
-  } catch (err) {
-    if (err.code === 'ENOENT') {
-      console.log(
-        {
-          error: err.message,
-          errno: err.errno,
-          code: err.code,
-          syscall: err.syscall,
-          path: err.path
-        });
+  const fileName = process.argv[2];
+  const text = process.argv[3];
+  fileStream.writeFile(fileName, text, 'utf-8', err => {
+    if (err) {
+      console.log(err);
     }
-  }
+  });
 }
