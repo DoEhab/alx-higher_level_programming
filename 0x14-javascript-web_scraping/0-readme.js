55fb94550a42e6d82a95a2079d9c19c2fb2587bc
dohaehab <dohaehab@gmail.com>
2025-02-11 23:42:43 +0200

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const fileStream = require('fs');
-if (process.argv[2]) {
-  const fileName = process.argv[2];
-  const text = process.argv[3];
-  fileStream.writeFile(fileName, text, 'utf-8', err => {
-    if (err) {
-      console.log(err);
-    }
-  });
-}
+const fileName = process.argv[2];
+const text = process.argv[3];
+fileStream.writeFile(fileName, text, err => {
+  if (err) {
+    console.log(err);
+  }
+});
