add1eed6aee1be11303b171a66d5619a453f9c57
dohaehab <dohaehab@gmail.com>
2025-02-12 22:29:56 +0200

    task 1

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
-request(url, function (error, response, body) {
+const url = process.argv[2];
+request.get(url, function (error, response, body) {
   if (!error) {
     let counter = 0;
     const data = JSON.parse(body);
