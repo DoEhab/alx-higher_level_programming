b57bb485d00ee83498941239bce98444d8271f9c
dohaehab <dohaehab@gmail.com>
2025-02-12 22:56:32 +0200

    task 0

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,6 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 request.get(url, function (error, response, body) {
   if (!error) {
-    const data = JSON.parse(body);
-    fs.writeFile(filePath, data);
+    fs.writeFile(filePath, body);
   }
 });
