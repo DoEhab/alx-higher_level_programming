77db87b5329b467a5689782447e0b540adc7f494
dohaehab <dohaehab@gmail.com>
2025-02-12 23:03:26 +0200

    task 0

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,5 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const filePath = process.argv[3];
-request.get(url, function (error, response, body) {
-  if (!error) {
-    fs.writeFile(filePath, body, (err) => {
-      if (err) {
-        console.error('File write error:', err);
-      }
-    });
-  }
-});
+request(url).pipe(fs.createWriteStream(filePath));
+
